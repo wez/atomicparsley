6c0161db6eb8a28b2b96f6693a575aa335befd42
Wez Furlong <wez@wezfurlong.org>
2020-07-01 13:00:58 -0700

    replace some latin1 (c) symbol with (c)

--- src/CDtoc.cpp
+++ src/CDtoc.cpp
@@ -15,7 +15,7 @@
     cannot, write to the Free Software Foundation, 59 Temple Place
     Suite 330, Boston, MA 02111-1307, USA.  Or www.fsf.org
 
-    Copyright ©2006-2007 puck_lock
+    Copyright (C)2006-2007 puck_lock
     with contributions from others; see the CREDITS file
                                                                    */
 //==================================================================//
